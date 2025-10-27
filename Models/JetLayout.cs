2d42d45f2136a671269e1bc5ddfd99181c0f6ba1
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-27 19:16:04 +0000

    Update JetLayout.cs to contain new data - old data shifted to LayoutCell model

--- Models/JetLayout.cs
+++ Models/JetLayout.cs
@@ -2,8 +2,7 @@ namespace JetInteriorApp.Models
 {
     public class JetLayout
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int? ComponentId { get; set; }
+        public int ConfigID { get; set; }
+        public int Components { get; set; }
     }
 }
