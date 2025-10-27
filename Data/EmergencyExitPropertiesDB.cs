e00b83b55892d43376bfa865c6de6f46bcb4d165
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-27 14:17:48 +0000

    Signage type and Accessibility features now nullable

--- Data/EmergencyExitPropertiesDB.cs
+++ Data/EmergencyExitPropertiesDB.cs
@@ -3,8 +3,8 @@ public class EmergencyExitPropertiesDB
     public int Id { get; set; }
     public int ComponentId { get; set; }
     public float ClearanceRadius { get; set; }
-    public string SignageType { get; set; }
-    public string AccessibilityFeatures { get; set; } // JSON
+    public string? SignageType { get; set; }
+    public string? AccessibilityFeatures { get; set; } // JSON
 
-    public InteriorComponentDB Component { get; set; }
+    public InteriorComponentDB? Component { get; set; }
 }
