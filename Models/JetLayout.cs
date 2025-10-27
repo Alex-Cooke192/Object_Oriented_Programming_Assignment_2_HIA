205df58de9993c5197cba0b5ff2e1a31b262be9c
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-27 13:53:25 +0000

    JetLayout Wrapper class Model added to enhance scalability & flexibility of JSON file parse

---
+++ Models/JetLayout.cs
@@ -0,0 +1,9 @@
+namespace JetInteriorApp.Models
+{
+    public class JetLayout
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int? ComponentId { get; set; }
+    }
+}
