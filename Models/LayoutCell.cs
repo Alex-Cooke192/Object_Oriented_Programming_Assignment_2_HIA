af72ee29b83b9b4c02d2404aa63f241a30949dc7
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-27 19:15:25 +0000

    Create LayoutCell.cs - represents 1 'seat' space on the jet, but can contain any interior component

---
+++ Models/LayoutCell.cs
@@ -0,0 +1,9 @@
+namespace JetInteriorApp.Models
+{
+    public class LayoutCell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int? ComponentId { get; set; }
+    }
+}
