1ae0ae62923e30ed10d6a852b2061ce90acc0d3a
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-30 13:01:59 +0000

    Added Load Propertiesbytype, to return only the data for the type of object

--- Services/JsonComponentPorter.cs
+++ Services/JsonComponentPorter.cs
@@ -41,6 +41,21 @@ namespace JetInteriorApp.Services
                 ComponentList.Add(full);
             }
 
+        private async Task<object> LoadPropertiesByType(Guid componentId, string type)
+        {
+            return type.ToLower() switch
+            {
+                "seat" => await _db.SeatProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "screen" => await _db.ScreenProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "table" => await _db.TableProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "kitchen" => await _db.KitchenProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "storage" => await _db.StorageCabinetProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "toilet" => await _db.ToiletProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "lighting" => await _db.LightingProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                "emergency_exit" => await _db.EmergencyExitProperties.FirstOrDefaultAsync(p => p.ComponentId == componentId),
+                _ => null
+            };
+        }
             return ComponentList;
         }
     }
