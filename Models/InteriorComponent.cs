79c218ed0891e6a54942457b52e78e0975769b22
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-30 12:13:46 +0000

    added interiorComponent object, data structure for an interior component to retrieve from the database

---
+++ Models/InteriorComponent.cs
@@ -0,0 +1,14 @@
+public class InteriorComponent
+{
+    public Guid Id { get; set; }
+    public Guid ConfigId { get; set; }
+    public string Type { get; set; } // e.g. "seat", "screen"
+    public string position { get; set; }
+    public string Material { get; set; }
+    public float Weight { get; set; }
+    public string Color { get; set; }
+    public string Tier { get; set; }
+    public string Accessibility { get; set; }
+
+    public object Properties { get; set; } // Holds subtype-specific data
+}
