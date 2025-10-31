29f8c44fd42446f27dd7d05aa4a11de0cf8bb5f4
Alex Cooke <30250508@blackpool.ac.uk>
2025-10-31 17:20:58 +0000

    corrected fields in jetconfig domain/business models

--- Models/JetConfig.cs
+++ Models/JetConfig.cs
@@ -2,8 +2,10 @@ public class ConfigEntry
 {
     public Guid ConfigID { get; set; } // Unique ID for this entry (could be same as ConfigID or separate)
     public Guid UserId { get; set; }  // Owner of the config
+    public string Name { get; set; }
+    public string Version { get; set; }
+    public string configJson { get; set; }
+    public bool ValidationStatus { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-    public string? Name { get; set; }
-    public object? JetLayout { get; set; } // The actual layout object
 }
