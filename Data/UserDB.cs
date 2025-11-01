4d0b909c009d76f8a5ca7d54bea1ef2e0c3f1ac3
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-01 19:17:10 +0000

    added userDB file - persistence layer model for user records in the database

---
+++ Data/UserDB.cs
@@ -0,0 +1,21 @@
+namespace JetInteriorApp.Data;
+using System.ComponentModel.DataAnnotations;
+
+public class UserDB
+{
+    [Key]
+    public Guid UserID { get; set; }
+
+    [Required]
+    public string Username { get; set; } = string.Empty; 
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    [Required]
+    public string Email { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    // Navigation property
+    public ICollection<JetConfigurationDB> Configurations { get; set; }
+}
