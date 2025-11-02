2b7d6e340cb5ebc7be00678af7621bc3cdd5f253
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-02 14:59:49 +0000

    Added fields that currently exist in business layer model 'jetConfiguration' to persistence model

--- Data/JetConfigurationDB.cs
+++ Data/JetConfigurationDB.cs
@@ -16,6 +16,8 @@ namespace JetInteriorApp.Data
         public Guid UserID { get; set; }
         public string Name { get; set; }
         public int SeatingCapacity { get; set; }
+        public string CabinDimensions { get; set; }
+        public int Version { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
