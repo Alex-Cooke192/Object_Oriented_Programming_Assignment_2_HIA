92f282d340f518aa915104e52b7760331cf4f2c2
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-04 13:51:20 +0000

    Added interface for Autheorisation repo - only purpose of this is to be injected into LoginViewModel to reduce coupling

---
+++ JIDS/Interfaces/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetInteriorApp.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<bool> ValidateUserAsync(string username, string plainPassword);
+        Task<bool> RegisterUserAsync(string username, string email, string plainPassword);
+    }
+
+}
