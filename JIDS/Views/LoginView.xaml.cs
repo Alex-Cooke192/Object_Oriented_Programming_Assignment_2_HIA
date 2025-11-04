74ce7e5f840f58071abba0a0062303fb64676316
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-04 17:03:24 +0000

    Removed database logic from this file - only dealw ith data binding for the login system

--- JIDS/Views/LoginView.xaml.cs
+++ JIDS/Views/LoginView.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using JetInteriorApp.ViewModels;
-using JetInteriorApp.Repositories;
-using JetInteriorApp.Interfaces;
-using JetInteriorApp.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace JetInteriorApp.Views
 {
@@ -13,21 +9,6 @@ namespace JetInteriorApp.Views
         public LoginView()
         {
             InitializeComponent();
-
-            // Build DbContext options 
-            var options = new DbContextOptionsBuilder<JetDbContext>()
-                .UseSqlite("DB_FILE_PATH")
-                .Options;
-
-            // Create context using options
-            var dbContext = new JetDbContext(options);
-
-            // Create database if it doesn't exist yet
-            dbContext.Database.EnsureCreated();
-
-            // Create repository and inject it into the ViewModel
-            IAuthRepository authRepository = new AuthRepository(dbContext);
-            DataContext = new LoginViewModel(authRepository);
         }
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
@@ -39,4 +20,5 @@ namespace JetInteriorApp.Views
         }
     }
 }
+
 
