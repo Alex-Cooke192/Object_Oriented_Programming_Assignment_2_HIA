17119311c9f96c9caa8707f6ef3b1b4208943407
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-04 10:16:18 +0000

    added basis of passwordHasher class - helper to support password logic for authentication system

---
+++ JIDS/Services/PasswordHasher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetInteriorApp.Services
+{
+    internal class PasswordHasher
+    {
+        public static string HashPassword(string plainPassword)
+        {
+            if (plainPassword == null)
+            {
+
+            }
+        }
+
+        public static bool VerifyPassword(string plainPassword, string hashedPassword)
+        {
+
+        }
+    }
+}
