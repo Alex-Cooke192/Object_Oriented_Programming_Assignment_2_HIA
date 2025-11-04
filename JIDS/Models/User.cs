ef2d5f471cdc409058aa24763d39f7db26156ea6
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-04 09:52:29 +0000

    Changed authentication system to old version (prior to persistence layer testing

--- JIDS/Models/User.cs
+++ JIDS/Models/User.cs
@@ -1,47 +1,4 @@
 using System;
-using BCrypt.Net;
-
-namespace JetInteriorApp.Models
-{
-    public class User
-    {
-        // Basic user fields
-        public int UserID { get; set; } // You can set manually for testing
-        public string Username { get; set; }
-        public string? Email { get; set; }
-        public DateTime CreatedAt { get; set; }
-
-        // Password storage (hashed)
-        public string PasswordHash { get; set; }
-
-        // Method to set password securely
-        public void SetPassword(string password)
-        {
-            if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentException("Password cannot be empty");
-
-            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password);
-        }
-
-        // Method to verify password
-        public bool VerifyPassword(string password)
-        {
-            if (string.IsNullOrEmpty(PasswordHash))
-                return false;
-
-            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
-        }
-
-        // For debugging/testing purposes
-        public override string ToString()
-        {
-            return $"UserID: {UserID}, Username: {Username}, Email: {Email}, CreatedAt: {CreatedAt}";
-        }
-    }
-}
-
-/*
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using BCrypt.Net; // Need to install BCrypt.Net-Next NuGet package for hashing
@@ -106,4 +63,3 @@ namespace JetInteriorApp.Models
         }
     }
 }
-*/
