b3e76225dce0e84d686292b41dfab78470b65d38
Alex Cooke <30250508@blackpool.ac.uk>
2025-11-04 15:34:47 +0000

    Added back-end of LoginView to create users db when logging in and pull the password from the password box - cannot do it directly for secure practices

--- JIDS/Views/LoginView.xaml.cs
+++ JIDS/Views/LoginView.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using JetInteriorApp.ViewModels;
+using JetInteriorApp.Repositories;
+using JetInteriorApp.Interfaces;
+using JetInteriorApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace JetInteriorApp.Views
 {
@@ -9,6 +13,21 @@ namespace JetInteriorApp.Views
         public LoginView()
         {
             InitializeComponent();
+
+            // Build DbContext options 
+            var options = new DbContextOptionsBuilder<JetDbContext>()
+                .UseSqlite("DB_FILE_PATH")
+                .Options;
+
+            // Create context using options
+            var dbContext = new JetDbContext(options);
+
+            // Create database if it doesn't exist yet
+            dbContext.Database.EnsureCreated();
+
+            // Create repository and inject it into the ViewModel
+            IAuthRepository authRepository = new AuthRepository(dbContext);
+            DataContext = new LoginViewModel(authRepository);
         }
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
