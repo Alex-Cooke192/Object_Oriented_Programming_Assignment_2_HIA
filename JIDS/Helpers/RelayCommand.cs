d82419b5f3d5092a94bda9a270579904a2f0c662
30250508 <30250508@blackpool.ac.uk>
2025-11-05 15:05:00 +0000

    RelayCommand now handles synchronous and asynchronous commands

--- JIDS/Helpers/RelayCommand.cs
+++ JIDS/Helpers/RelayCommand.cs
@@ -1,27 +1,48 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace JetInteriorApp.Helpers
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action<object?> _execute;
+        private readonly Action<object?>? _executeSync;
+        private readonly Func<object?, Task>? _executeAsync;
         private readonly Predicate<object?>? _canExecute;
 
         public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
         {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _executeSync = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Func<object?, Task> executeAsync, Predicate<object?>? canExecute = null)
+        {
+            _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
             _canExecute = canExecute;
         }
 
         public bool CanExecute(object? parameter)
-        {
-            return _canExecute == null || _canExecute(parameter);
-        }
+            => _canExecute?.Invoke(parameter) ?? true;
 
+        // ICommand requires void return type
         public void Execute(object? parameter)
         {
-            _execute(parameter);
+            if (_executeAsync != null)
+            {
+                // fire-and-forget to keep ICommand happy
+                _ = _executeAsync(parameter);
+            }
+            else
+            {
+                _executeSync!(parameter);
+            }
+        }
+
+        // Extra helper: allows tests to await async commands
+        public Task? ExecuteAsync(object? parameter)
+        {
+            return _executeAsync?.Invoke(parameter);
         }
 
         public event EventHandler? CanExecuteChanged
@@ -31,4 +52,3 @@ namespace JetInteriorApp.Helpers
         }
     }
 }
-
