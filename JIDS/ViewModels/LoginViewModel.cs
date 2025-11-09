f883862878d76676a50e7cb82a7d8c41a30e3d0f
Isobel Pritchard <isobelgp@icloud.com>
2025-11-09 16:12:27 +0000

    Refactored login to be a page, so that MVVM best practices are followed.

--- JIDS/ViewModels/LoginViewModel.cs
+++ JIDS/ViewModels/LoginViewModel.cs
@@ -1,25 +1,30 @@
 ﻿using JetInteriorApp.Helpers;
 using JetInteriorApp.Interfaces;
-using JetInteriorApp.Repositories;
-using JetInteriorApp.Views;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
-using System.Linq;
+using JetInteriorApp.Data;
+using JetInteriorApp.Models;
+using JetInteriorApp.Services.Configuration;
+using System.Windows;
 
 namespace JetInteriorApp.ViewModels
 {
     public class LoginViewModel : INotifyPropertyChanged
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IUserSessionService _userSession;
+        private readonly INavigationService _navigationService;
 
-        public LoginViewModel(IAuthRepository authRepository)
+        public LoginViewModel(IAuthRepository authRepository, IUserSessionService userSession, INavigationService navigationService)
         {
             _authRepository = authRepository;
-            LoginCommand = new RelayCommand(_ => LoginAsync());
-            RegisterCommand = new RelayCommand(_ => RegisterAsync());
+            _userSession = userSession;
+            _navigationService = navigationService;
+
+            LoginCommand = new RelayCommand(async _ => await LoginAsync());
+            RegisterCommand = new RelayCommand(async _ => await RegisterAsync());
         }
 
         private string _username = string.Empty;
@@ -55,17 +60,28 @@ namespace JetInteriorApp.ViewModels
 
         private async Task LoginAsync()
         {
-            var user = await _authRepository.ValidateUserAsync(Username, Password);
+            var userDb = await _authRepository.ValidateUserAsync(Username, Password);
 
-            if (user != null)
+            if (userDb != null)
             {
-                StatusMessage = $"Welcome, {user.Username}!";
+                StatusMessage = $"Welcome, {userDb.Username}!";
 
-#if !DEBUG_TESTS
-        var mainWindow = new MainWindow(user.UserID);
-        mainWindow.Show();
-        Application.Current.Windows.OfType<LoginView>().FirstOrDefault()?.Close();
-#endif
+                // Create domain User and set session
+                var domainUser = User.FromExisting(userDb.UserID, userDb.Username, userDb.Email ?? string.Empty, userDb.PasswordHash ?? string.Empty, userDb.CreatedAt);
+                _userSession.SetCurrentUser(domainUser);
+
+                // Register user-scoped configuration repository/manager in Application resources
+                if (Application.Current.Resources["DbContext"] is JetDbContext db)
+                {
+                    var repo = new JsonConfigurationRepository(db, userDb.UserID);
+                    var manager = new ConfigurationManager(repo, userDb.UserID);
+
+                    Application.Current.Resources["ConfigurationRepository"] = repo;
+                    Application.Current.Resources["ConfigurationWriter"] = manager;
+                }
+
+                // Navigate to configuration list (MainWindow will host the view inside its frame)
+                _navigationService.NavigateTo("ConfigurationList");
             }
             else
             {
@@ -90,4 +106,3 @@ namespace JetInteriorApp.ViewModels
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
-
