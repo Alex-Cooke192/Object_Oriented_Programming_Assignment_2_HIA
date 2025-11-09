452ce833e357847f936d351e2270a6b57c4708d9
Isobel Pritchard <isobelgp@icloud.com>
2025-11-09 16:50:27 +0000

    Updated tests as necessary.

--- Tests/obj/Release/net8.0-windows/JetInteriorApp.Tests.AssemblyInfo.cs
+++ Tests/obj/Release/net8.0-windows/JetInteriorApp.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JetInteriorApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93d04ae6dcdae9dcb6fbe496105097de2460045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+692b49d40310d6cccf4c2d41f1a1085082331158")]
 [assembly: System.Reflection.AssemblyProductAttribute("JetInteriorApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JetInteriorApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
